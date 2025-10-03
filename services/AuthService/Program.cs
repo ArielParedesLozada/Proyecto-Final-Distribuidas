003bfacc8b826f709cf75e5e12f0c06af46858d9
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-10-03 11:29:44 -0500

    Estructura sin migraciones

--- services/AuthService/Program.cs
+++ services/AuthService/Program.cs
@@ -1,15 +1,51 @@
-// using AuthService.Services;
+using System.Text;
+using AuthService.Data.Databases;
+using AuthService.Data.Repository;
+using AuthService.Domain;
+using AuthService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddGrpc();
+// Add services to the container
+DotNetEnv.Env.Load();
+var JWT_SECRET = Environment.GetEnvironmentVariable("JWT_SECRET")!;
+var JWT_TIME = double.Parse(Environment.GetEnvironmentVariable("JWT_TIME")!);
+var CONNECTION_STRING = Environment.GetEnvironmentVariable("CONNECTION_STRING")!;
+var LOCAL_IP = Environment.GetEnvironmentVariable("LOCAL_IP")!;
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddDbContext<AppDatabase>(options => options.UseNpgsql(CONNECTION_STRING));
+builder.Services.AddScoped<IRepository<User>, Repository<User>>();
+builder.Services.AddScoped<JWTAuthService>(sp =>
+{
+    var repo = sp.GetRequiredService<IRepository<User>>();
+    return new JWTAuthService(JWT_SECRET, JWT_TIME, repo, LOCAL_IP);
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer((options) =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = LOCAL_IP,
+        ValidAudience = LOCAL_IP,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWT_SECRET))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<JWTAuthService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
