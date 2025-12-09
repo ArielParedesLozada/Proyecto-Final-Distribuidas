acfc8465c156096b70ad2bbb451b85c23447d649
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-12-09 01:33:49 -0500

    Dockerizacion funcional/// TODO: MALDITA SEA SE ME OVLIDO EL FRONT

--- docker/postgres-citus/init/coordinator/01-register-workers.sql
+++
@@ -1,3 +0,0 @@
-SELECT citus_set_coordinator_host('citus_coordinator_ad', 5432);
-SELECT citus_add_node('citus_worker1_ad', 5432);
-SELECT citus_add_node('citus_worker2_ad', 5432);
--- docker/postgres-citus/init/worker/02-pg-hba-override.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Añadiendo reglas a pg_hba.conf..."
-
-cat <<EOF >> /var/lib/postgresql/data/pg_hba.conf
-
-# Reglas añadidas manualmente
-host all all 0.0.0.0/0 trust
-host all all ::/0 trust
-EOF
